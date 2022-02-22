ca6a4f58a257ac30ad49e45a8bd5b2c48d4e6197
Peach <cutiekeek@gmail.com>
2022-02-22 15:15:19 +0900

    removed unused comments from main.py

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers import post, user, auth, vote
 from .config import settings
 
-# models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
 origins = ['*']
@@ -14,7 +12,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(post.router)
